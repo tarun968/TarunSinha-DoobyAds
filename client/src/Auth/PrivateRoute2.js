ab613b9cc59a4da740059e2ad0736ea034197657
Tarun U. Sinha <tarunsinha968@gmail.com>
2022-11-21 20:52:23 +0530

    fr

--- client/src/Auth/PrivateRoute2.js
+++ client/src/Auth/PrivateRoute2.js
@@ -2,11 +2,11 @@ import React from "react";
 import { isAuthenticated } from "./apicalls";
 import { useNavigate, Outlet ,Navigate} from "react-router-dom";
 
-const PrivateRoute = () => {
+const PrivateRoute2 = () => {
     let users = isAuthenticated();
     return (
-        users ? <Navigate to ='/'/> :<Outlet />
+        users ?  <Navigate to ='/Images'/> : <Outlet/> 
     )
 }
 
-export default PrivateRoute
+export default PrivateRoute2
--- client/src/Routes.js
+++ client/src/Routes.js
@@ -2,13 +2,12 @@ import React from "react";
 import { Fragment } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signin from "./Auth/sgnin";
+import PrivateRoute2 from "./Auth/PrivateRoute2";
 import App from "./App"
-// import Fruits from "./Fruits";
 import Signup from "./Auth/signup";
 import ImageForm from "./CRUD/addimage";
 import ImageUploaded from "./CRUD/images";
-// import UpdateFruits from "./Updatefruits";
-// import Addfruit from "./Addfruit";
+import PrivateRoute from "./Auth/PrivateRoutes";
 
 export default function Routing() {
     return (
@@ -16,11 +15,19 @@ export default function Routing() {
             <Fragment>
                 <Routes>
                     <Route path="/" element={<App />}></Route>
-                    <Route path="/Signin" element={<Signin />}></Route>
+                    <Route element={<PrivateRoute />}>
+                        <Route element={<Signin />} path='/Signin' />
+                        {/* <Route element = {<Login/>} path='/' /> */}
+                    </Route>
+                    {/* <Route element={<PrivateRoute2 />}> */}
+                        {/* <Route element={<Signin />} path='/Images' /> */}
+                        {/* <Route element={<Signin />} path='/Add-Image' /> */}
+                    {/* </Route> */}
+                    {/* <Route path="/Signin" element={<Signin />}></Route> */}
                     <Route path="/Images" element={<ImageUploaded />}></Route>
-                    <Route path="/Add-image" element={<ImageForm />}></Route> 
+                    <Route path="/Add-image" element={<ImageForm />}></Route>
                     {/* <Route path="/fruit/update/:fruitId" element={<UpdateFruits />}></Route> */}
-                    <Route path="/Signup" element={<Signup />}></Route> 
+                    <Route path="/Signup" element={<Signup />}></Route>
                 </Routes>
             </Fragment>
         </BrowserRouter>
