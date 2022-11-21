400b24929e84bf71b8a6d78d0959de89cf5e09ab
Tarun U. Sinha <tarunsinha968@gmail.com>
2022-11-21 21:54:57 +0530

    fr

--- client/src/CRUD/imagehelper.js
+++ client/src/CRUD/imagehelper.js
@@ -5,11 +5,8 @@ import { getOneImage } from "./imgapicalls";
 export default function ImageHelper({user}) {
     // console.log(userid)
     console.log(user)
-    // const {Token} = isAuthenticated();
-    // const NameImage  = getOneImage(Token,user.userid,user.ImageId)
-    // console.log(user.ImageId)
-    return  (
-        <img src={`https://fair-teal-moth-tux.cyclic.app/Image/${user.userid}/photo/${user.id}/`} 
+        return  (
+        <img src={`http://localhost:4000/Image/${user.userid}/photo/${user.id}/`} 
         style = {{height:'100 px'}}
         />
     )
--- users/userFunc.js
+++ users/userFunc.js
@@ -129,7 +129,7 @@ exports.getPhoto = async (req, res, next) => {
             }
         })
     if (imagesarray) {
-        console.log("images array",)
+        console.log("images array",imagesarray[0].ImageProduct[0].contentType)
         res.set('Content-Type', imagesarray[0].ImageProduct[0].contentType)
         return res.send(imagesarray[0].ImageProduct[0].data)
     }
